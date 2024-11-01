cc492df1c42d235178329c138514de6e233bef99
king0jin <et0709@naver.com>
2024-11-01 20:48:51 +0900

    Book모델 생성

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+#도서 정보를 저장하기위한 모델 = 테이블
+class Book(models.Model):
+    book_id = models.IntegerField(primary_key=True)
+    book_name = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    price = models.IntegerField()
+    published_date = models.DateField()
