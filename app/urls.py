f4f3b7c166f8471269a22e945c2376c968d9bd70
king0jin <et0709@naver.com>
2024-11-01 21:55:10 +0900

    데이터 삽입

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     #1. hello/요청이 왔을 때, helloAPI함수 호출
     path("hello/", helloAPI),
     #3. books/요청이 왔을 때, booksAPI함수 호출
-    path("books/", booksAPI), #CRUD - 전체 데이터 조회
+    path("books/", booksAPI), #CRUD - 전체 데이터 조회, 데이터 삽입
     #4. book/book_id 요청이 왔을 때, bookAPI함수 호출
-    path("book/<int:book_id>", bookAPI) #CRUD - 특정 데이터 조회
+    path("book/<int:book_id>", bookAPI), #CRUD - 특정 데이터 조회   
 ]
--- app/views.py
+++ app/views.py
@@ -14,14 +14,23 @@ from rest_framework.generics import get_object_or_404
 def helloAPI(request):
     return Response("hello!")
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def booksAPI(request):
-    #테이블의 전체 테이블 가져오기
-    books = Book.objects.all()
-    #가져온 데이터를 JSON문자열로 변환
-    serializer = BookSerializer(books, many=True)
-    #JSON으로 출력
-    return Response(serializer.data, status.HTTP_200_OK)
+    if request.method == 'GET':
+        #테이블의 전체 테이블 가져오기
+        books = Book.objects.all()
+        #가져온 데이터를 JSON문자열로 변환
+        serializer = BookSerializer(books, many=True)
+        #JSON으로 출력
+        return Response(serializer.data, status.HTTP_200_OK)
+    elif request.method == 'POST':
+        #클라이언트가 입력한 데이터를 모델로 변환
+        client_data = BookSerializer(data = request.data)
+        #유효성 검사
+        if client_data.is_valid():
+            client_data.save() #유효성 검사를 통과라면 데이터베이스에 반영 
+            return Response(client_data.data, status=status.HTTP_201_CREATED)
+        return Response(client_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 def bookAPI(request, book_id):
